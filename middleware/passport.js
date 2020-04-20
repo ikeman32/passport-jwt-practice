d651d3ab504a8d716a07f50b6bdf2f862db335c9
David Isakson <ikeman32@yahoo.com>
2020-04-20 10:43:12 -0700

    Fixed typo

--- middleware/passport.js
+++ middleware/passport.js
@@ -1,28 +1,29 @@
-require('dotenv').config();
-const {Strategy, ExtractJwt} = require('passport-jwt');
+require("dotenv").config();
+const { Strategy, ExtractJwt } = require("passport-jwt");
 
 const secret = process.env.JWT_SECRET;
 
-const User = require('../models/auth-model');
+const User = require("../models/auth-model");
 
 const opts = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey: secret
+  secretOrKey: secret,
 };
 
-module.exports = passport => {
+module.exports = (passport) => {
   passport.use(
-      new Strategy(opts, (payload, done) => {
-           User.findById(payload.id)
-               .then(user => {
-                   if(user){
-                     return done(null, {
-                         id: user.id,
-                         username: user.username
-                     });
-                   }
-                   return done(null, false);
-                }).catch(err => console.error(err));
+    new Strategy(opts, (payload, done) => {
+      User.findById(payload.id)
+        .then((user) => {
+          if (user) {
+            return done(null, {
+              id: user.id,
+              username: user.username,
             });
-         );
-   };
+          }
+          return done(null, false);
+        })
+        .catch((err) => console.error(err));
+    })
+  );
+};
